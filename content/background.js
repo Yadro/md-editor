72a28927f5067d5681460e9ffb9121214108d870
yadro <yadrone@gmail.com>
2016-06-17 21:13:16 +0300

    init

---
+++ content/background.js
@@ -0,0 +1,8 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  var window = chrome.app.window.create('index.html', {
+    id: 'main',
+    bounds: { width: 620, height: 500 },
+    innerBounds: { minWidth: 300, minHeight: 300 }
+  });
+
+});
---
+++ src/app.js
@@ -0,0 +1,7 @@
+var SimpleMDE = require('simplemde');
+
+new SimpleMDE({
+  element: document.getElementById("editor"),
+  spellChecker: false,
+  autoDownloadFontAwesome: false
+});
