d5cb110a43c96fc48998fd73895689acd25d725b
yadro <yadrone@gmail.com>
2016-06-24 23:00:58 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,34 @@
 "use strict";
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 
-  entry: "./src/app.tsx",
+  entry: __dirname + "/src/app.tsx",
   output: {
-    path: __dirname + "/build_2",
-    filename: "app.js"
+    path: __dirname + "/build2",
+    filename: 'app.js'
   },
 
   module: {
-    loaders: [{
-      test: /\.tsx?$/,
-      path: './src',
-      loader: 'ts-loader'
-    }]
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: [/node_modules/]
+      }/*, {
+        test: /\.css$/,
+        loader: 'style!css',
+      }, {
+        test: /\.(png|jpg|otf|eot|svg|ttg|woff2?)$/,
+        loader: 'file-loader',
+        include: [/node_modules/]
+      }*/
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.ts', '.tsx', '.js','.jsx'],
+    modulesDirectories: ['node_modules']
   },
 
   //watch: true,
@@ -22,4 +37,17 @@ module.exports = {
   },
 
   devtool: "source-map",
+
+  plugins: [
+    new CopyWebpackPlugin([
+      {from: 'index.html'},
+      // {from: 'content/**/*'},
+      // {from: './node_modules/font-awesome/fonts/*'},
+      {from: 'node_modules/simplemde/dist/simplemde.min.css', to: 'style'},
+      {from: 'node_modules/react-tagsinput/react-tagsinput.css', to: 'style'},
+      {from: 'node_modules/font-awesome/css/font-awesome.min.css', to: 'style'}
+    ], {
+      copyUnmodified: false
+    })
+  ]
 };
